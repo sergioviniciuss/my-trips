bf52e7d2995901013c9dc3b75c8a79b470f7c2b2
Sérgio Lucena <sergioviniciuss@gmail.com>
2021-07-04 12:44:23 +0200

    style(graphql client): Formats code with prettier

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST || ''
 
-const client = new GraphQLClient( endpoint, {
+const client = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
   }
