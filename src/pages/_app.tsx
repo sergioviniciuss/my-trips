834528def846d20ffe21b5fcde9316004b4b799a
Sérgio Lucena <sergioviniciuss@gmail.com>
2021-07-17 19:14:02 +0200

    feat(seo): Adds google site verification tag for google search console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,15 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="6B1N7ON18nu09vz0i9MQleInWn0VzXBA33KT_EYu7Zk"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link 
-          rel="stylesheet" 
+        <link
+          rel="stylesheet"
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin=""
